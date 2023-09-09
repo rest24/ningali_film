0289d79109ec587f04e36029effeff8e4961d20c
rest24 <restu.andriyana.suhendar@gmail.com>
2023-09-10 04:08:01 +0700

    nambah film baru

--- data.js
+++ data.js
@@ -1,11 +1,21 @@
 // Data kartu aksi
 const actionData = [
     {
+        title: "Transformers: Rise of the Beasts",
+        director: "Steven Caple Jr",
+        writer: "Joby Harold, Darnell Metayer, Josh Petter",
+        stars: "Anthony Ramos, Dominique Fishback, Luna Lauren Velez",
+        image: "img/transformer.jpg",
+        watch: "https://tutwuri.id/gOa6ZGM3",
+        download: "https://drive.google.com/file/d/1MbA5hHUg8Zx-13_dUYotTV5pFpyCPMlB/view?usp=drivesdk",
+        trailer: "https://www.youtube.com/watch?v=itnqEauWQZM"
+    },
+    {
         title: "Fast X",
         director: "Louis Leterrier",
         writer: "Dan Mazeau, Justin Lin, Zach Dean",
         stars: "Vin Diesel, Michelle Rodriguez, Jason Statham",
-        image: "fastx.jpg",
+        image: "img/fastx.jpg",
         watch: "https://tutwuri.id/OhgBytq",
         download: "https://drive.google.com/file/d/1EXUKWuLbhOymDQxlgMdf94alUzkDAAiy/view?usp=drivesdk",
         trailer: "https://www.youtube.com/watch?v=eoOaKN4qCKw"
@@ -20,7 +30,7 @@ const horrorData = [
         director: "Corin Hardy",
         writer: "Gary Dauberman, James Wan",
         stars: "Demián Bichir, Taissa Farmiga, Jonas Bloquet",
-        image: "nun.jpg",
+        image: "img/nun.jpg",
         watch: "https://tutwuri.id/KJm0Ssvvr8fz",
         download: "https://drive.google.com/file/d/1DUJOXbLJ8GreHo_fsxMwkMCS4xZJpoEH/view?usp=drivesdk",
         trailer: "https://youtu.be/pzD9zGcUNrw?si=K-d2JR2QXsRjgPyU"
@@ -30,7 +40,7 @@ const horrorData = [
         director: "John R. Leonetti",
         writer: "Gary Dauberman",
         stars: "Ward Horton, Annabelle Wallis, Alfre Woodart",
-        image: "annabelle.jpg",
+        image: "img/annabelle.jpg",
         watch: "https://tutwuri.id/PSquKey5BE",
         download: "https://drive.google.com/file/d/1--RvOkBTO4lRTlGW8-I7Frb8OAMOcVz8/view?usp=drivesdk",
         trailer: "https://youtu.be/paFgQNPGlsg?si=YDCG30VNc8ijkENW"
