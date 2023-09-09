00531dac188b628ded2ece8e040993696d63c114
rest24 <restu.andriyana.suhendar@gmail.com>
2023-09-09 19:47:45 +0700

    menambah file js

---
+++ data.js
@@ -0,0 +1,39 @@
+// Data kartu aksi
+const actionData = [
+    {
+        title: "Fast X",
+        director: "Louis Leterrier",
+        writer: "Dan Mazeau, Justin Lin, Zach Dean",
+        stars: "Vin Diesel, Michelle Rodriguez, Jason Statham",
+        image: "fastx.jpg",
+        watch: "https://tutwuri.id/OhgBytq",
+        download: "https://drive.google.com/file/d/1EXUKWuLbhOymDQxlgMdf94alUzkDAAiy/view?usp=drivesdk",
+        trailer: "https://www.youtube.com/watch?v=eoOaKN4qCKw"
+    },
+    // Tambahkan data kartu aksi lainnya di sini
+];
+
+// Data kartu horor
+const horrorData = [
+    {
+        title: "The Nun",
+        director: "Corin Hardy",
+        writer: "Gary Dauberman, James Wan",
+        stars: "Demián Bichir, Taissa Farmiga, Jonas Bloquet",
+        image: "nun.jpg",
+        watch: "https://tutwuri.id/KJm0Ssvvr8fz",
+        download: "https://drive.google.com/file/d/1DUJOXbLJ8GreHo_fsxMwkMCS4xZJpoEH/view?usp=drivesdk",
+        trailer: "https://youtu.be/pzD9zGcUNrw?si=K-d2JR2QXsRjgPyU"
+    },
+    {
+        title: "Annabelle",
+        director: "John R. Leonetti",
+        writer: "Gary Dauberman",
+        stars: "Ward Horton, Annabelle Wallis, Alfre Woodart",
+        image: "annabelle.jpg",
+        watch: "https://tutwuri.id/PSquKey5BE",
+        download: "https://drive.google.com/file/d/1--RvOkBTO4lRTlGW8-I7Frb8OAMOcVz8/view?usp=drivesdk",
+        trailer: "https://youtu.be/paFgQNPGlsg?si=YDCG30VNc8ijkENW"
+    },
+    // Tambahkan data kartu horor lainnya di sini
+];
